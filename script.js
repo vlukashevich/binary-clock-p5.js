6180e6e0d94e18a1cbe4b9e6767e91e424413054
Vladislav Lukashevich <43114166+vlukashevich@users.noreply.github.com>
2019-08-11 15:15:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,62 @@
-//array of numbers that show how many rows of bits (circles) we have in each column
-let rows = [2, 4, 3, 4, 3, 4];
-let grid;
+let cols = [2, 4, 3, 4, 3, 4];
+let len;
+
+let bright;
+let dark;
+let index = 2;
+
+let w;
+let side_margin = 50;
+let grid_margin = 5;
+
+function get_time () {
+  let sec = second();
+  let s = (sec > 9) ? sec.toString() : "0" + sec.toString();
+  
+  let min = minute();
+  let m = (min > 9) ? min.toString() : "0" + min.toString();
+  
+  let hr = hour();
+  let h = (hr > 9) ? hr.toString() : "0" + hr.toString();
+  
+  return h + m + s;
+}
 
 function setup() {
-  //colorMode(HSB, 100, 100, 100, 1); // for rainbow colors
   createCanvas(600, 600);
   
-  //making a "grid" of circles representing each bit of each digit
-  grid = new Array(6);  
-  for (let i = 0; i < 6; i++) {
-    grid[i] = new Array(rows[i]);
-    
-    for (let j = 0; j < rows[i]; j++) {
-      grid[i][j] = 0;  
-    }
-  }
+  bright = [color(0, 255, 50), color(0, 255, 255), color(255, 0, 255)];
+  dark = [color(0, 124, 50), color(0, 120, 120), color(120, 0, 120)];
+  
+  len = cols.length;
+  w = (width - 2 * side_margin) / len;
 }
+
 
 function draw() {
   background(51);
-  let w = width/6;
-  let time = getTime();
+  let time = get_time();
   
-  //loop through all 6 digits of time string
-  for (let i = 0; i < 6; i++) {
-    //get ith digit
-    let num = parseInt(time[i]);
-    
-    //get binary representation of ith digit
-    let binary = num.toString(2);  
-    
-    // reverse binary string
-    binary = binary.split("").reverse().join("");
-    
-    //set the value of each element of the array   
-    for (let j = 0; j < rows[i]; j++) {
-      if (!binary[j]) {
-        grid[i][j] = 0;  
-      } else {  
-        grid[i][j] = parseInt(binary[j]);    
-      }  
-    }
-  }
+  noStroke();
   
-  
-  //draw binary clock
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < rows[i]; j++) {
-      push();
-      noStroke();
+  for (var i = 0; i < len; i++) {
+    let x = side_margin + w * (i + 0.5);
+    
+    let bin = parseInt(time[i]).toString(2);
+    bin = bin.split("").reverse().join("");
+    
+    for (var j = 0; j < cols[i]; j++) {
+      let y = height - (side_margin + w * (j + 0.5));
       
-      //we need hue, alpha and bright for HSB color mode in case we want rainbow coloring
-      let hue = map(i, 0, 5, 0, 80);
-      let alpha;
-      let bright;
+      let d = w - 2 * grid_margin;
       
-      if (grid[i][j] == 0) {
-        bright = 60;
-        alpha = 0.6;
-        fill(120, 0, 120);
+      if (bin.charAt(j) == '1') {
+        fill(bright[index]);  
       } else {
-        alpha = 1;
-        bright = 100;
-        fill(255, 0, 255);
+        fill(dark[index]);  
       }
       
-      //x & y coordinates of current circle
-      let x = w * (i + 0.5);
-      let y = w * (6 - j - 1  + 0.5);
-      
-      //fill(hue, 100, bright, alpha);
-      
-      ellipse(x, y, w - 10, w - 10);
-      pop();
+      circle(x, y, d/2);
     }
   }
-  
-  // draw time in decimal
-  textSize(30);
-  time = time[0] + time[1] + ":" + time[2] + time[3] + ":" + time[4] + time[5];
-  fill(255, 0, 255);
-  text(time, 450, 50);
-}
-
-//self-made function to get time string in hhmmss format
-function getTime () {
-  let h = (hour().toString().length == 2) ? hour().toString() : "0" + hour().toString();
-  let m = (minute().toString().length == 2) ? minute().toString() : "0" + minute().toString();
-  let s = (second().toString().length == 2) ? second().toString() : "0" + second().toString();
-  
-  let time = h + m + s;
-  
-  return time;
 }
